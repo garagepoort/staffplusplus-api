69d3d20b158b0e2240428befb42bbc1973ffb04d
David Maes <david.s.maes@gmail.com>
2021-04-13 23:24:06 +0200

    Add staff mode switch event

---
+++ src/main/java/net/shortninja/staffplusplus/staffmode/SwitchStaffModeEvent.java
@@ -0,0 +1,63 @@
+package net.shortninja.staffplusplus.staffmode;
+
+import org.bukkit.Location;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+import java.util.UUID;
+
+public class SwitchStaffModeEvent extends Event {
+
+
+    private static final HandlerList HANDLERS = new HandlerList();
+
+    private final String playerName;
+    private final UUID playerUuid;
+    private final Location location;
+    private final String serverName;
+    private final String fromMode;
+    private final String toMode;
+
+    public SwitchStaffModeEvent(String playerName, UUID playerUuid, Location location, String serverName, String fromMode, String toMode) {
+        this.playerName = playerName;
+        this.playerUuid = playerUuid;
+        this.location = location;
+        this.serverName = serverName;
+        this.fromMode = fromMode;
+        this.toMode = toMode;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public UUID getPlayerUuid() {
+        return playerUuid;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public String getServerName() {
+        return serverName;
+    }
+
+    public String getFromMode() {
+        return fromMode;
+    }
+
+    public String getToMode() {
+        return toMode;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return HANDLERS;
+    }
+
+    public static HandlerList getHandlerList() {
+        return HANDLERS;
+    }
+
+}
