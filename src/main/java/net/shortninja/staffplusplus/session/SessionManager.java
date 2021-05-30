9e7cad4ca9d378c8bf7b0d5277f8e3f562b884a1
David Maes <david.s.maes@gmail.com>
2021-05-30 21:59:26 +0200

    Add sessionmanager methods

--- src/main/java/net/shortninja/staffplusplus/session/SessionManager.java
+++ src/main/java/net/shortninja/staffplusplus/session/SessionManager.java
@@ -1,8 +1,13 @@
 package net.shortninja.staffplusplus.session;
 
+import java.util.Collection;
 import java.util.UUID;
 
 public interface SessionManager {
 
     IPlayerSession get(UUID uuid);
+
+    Collection<? extends IPlayerSession> getAll();
+
+    Collection<? extends IPlayerSession> getOnlineStaffMembers();
 }
