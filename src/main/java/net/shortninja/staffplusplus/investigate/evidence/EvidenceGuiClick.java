5f478faf334436776672c6dc633f33d4f9ca5bf2
David Maes <david.s.maes@gmail.com>
2021-07-28 20:26:50 +0200

    Update evidenceGuiClick

--- src/main/java/net/shortninja/staffplusplus/investigate/evidence/EvidenceGuiClick.java
+++ src/main/java/net/shortninja/staffplusplus/investigate/evidence/EvidenceGuiClick.java
@@ -1,11 +1,10 @@
 package net.shortninja.staffplusplus.investigate.evidence;
 
-import net.shortninja.staffplusplus.session.SppPlayer;
 import org.bukkit.entity.Player;
 
 public interface EvidenceGuiClick {
 
-    void onClick(Player player, SppPlayer target, int id, Runnable goBack);
-
     String getType();
+
+    String getAction(Player player, int evidenceId, String backAction);
 }
