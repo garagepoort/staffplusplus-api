8c58a93da44fb755b59d4928221d520218b7ac0c
David Maes <david.s.maes@gmail.com>
2021-08-05 02:00:33 +0200

    Add vanish events

---
+++ src/main/java/net/shortninja/staffplusplus/vanish/VanishOffEvent.java
@@ -0,0 +1,36 @@
+package net.shortninja.staffplusplus.vanish;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+public class VanishOffEvent extends Event {
+
+    private static final HandlerList HANDLERS = new HandlerList();
+
+    private final VanishType type;
+    private final Player player;
+
+    public VanishOffEvent(VanishType type, Player player) {
+        this.type = type;
+        this.player = player;
+    }
+
+    public VanishType getType() {
+        return type;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return HANDLERS;
+    }
+
+    public static HandlerList getHandlerList() {
+        return HANDLERS;
+    }
+
+}
