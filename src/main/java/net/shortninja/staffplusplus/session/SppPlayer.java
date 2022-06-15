561f5fa0ab4ca89048afb1a4398f7dd0d832ec80
DavidMaes <david.s.maes@gmail.com>
2022-06-15 12:46:45 +0200

    Add nightvision to sppPlayer

--- src/main/java/net/shortninja/staffplusplus/session/SppPlayer.java
+++ src/main/java/net/shortninja/staffplusplus/session/SppPlayer.java
@@ -1,7 +1,10 @@
 package net.shortninja.staffplusplus.session;
 
+import org.bukkit.GameMode;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 import java.util.UUID;
 
@@ -37,5 +40,18 @@ public class SppPlayer extends SppInteractor {
             throw new RuntimeException("Cannot retrieve bukkit player. Player is offline");
         }
         return player;
+    }
+
+    public void turnNightVisionOn() {
+        player.removePotionEffect(PotionEffectType.NIGHT_VISION);
+        player.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, Integer.MAX_VALUE, 128, false, false));
+    }
+
+    public void turnNightVisionOff() {
+        player.removePotionEffect(PotionEffectType.NIGHT_VISION);
+    }
+
+    public void setGameMode(GameMode gameMode) {
+        player.setGameMode(gameMode);
     }
 }
