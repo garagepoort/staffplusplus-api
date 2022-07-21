db3149a8b36b38fdf36cb230701aa8cee1163691
DavidMaes <david.s.maes@gmail.com>
2022-07-21 23:30:36 +0200

    Fix issue with warning filters

--- src/main/java/net/shortninja/staffplusplus/warnings/WarningFilters.java
+++ src/main/java/net/shortninja/staffplusplus/warnings/WarningFilters.java
@@ -20,58 +20,58 @@ public class WarningFilters extends SqlFilters {
         private List<ISqlFilter> warningFilters = new ArrayList<>();
 
         public WarningFiltersBuilder id(int id) {
-            this.warningFilters.add(new SqlFilter<>(id, Types.INTEGER, "sp_warnings.id"));
+            this.warningFilters.add(new SqlFilter<>(id, Types.INTEGER, "w.id"));
             return this;
         }
 
         public WarningFiltersBuilder severity(String severity) {
-            this.warningFilters.add(new SqlFilter<>(severity, Types.VARCHAR, "sp_warnings.severity"));
+            this.warningFilters.add(new SqlFilter<>(severity, Types.VARCHAR, "w.severity"));
             return this;
         }
 
         public WarningFiltersBuilder anyOfSeverity(List<String> severities) {
-            this.warningFilters.add(new SqlFilter<>(severities, Types.VARCHAR, "sp_warnings.severity"));
+            this.warningFilters.add(new SqlFilter<>(severities, Types.VARCHAR, "w.severity"));
             return this;
         }
 
         public WarningFiltersBuilder culprit(SppPlayer culprit) {
-            this.warningFilters.add(new SqlFilter<>(culprit.getId().toString(), Types.VARCHAR, "sp_warnings.player_uuid"));
+            this.warningFilters.add(new SqlFilter<>(culprit.getId().toString(), Types.VARCHAR, "w.player_uuid"));
             return this;
         }
 
 
         public WarningFiltersBuilder culpritName(String culpritName) {
-            this.warningFilters.add(new SqlFilter<>(culpritName, Types.VARCHAR, "sp_warnings.player_name"));
+            this.warningFilters.add(new SqlFilter<>(culpritName, Types.VARCHAR, "w.player_name"));
             return this;
         }
 
         public WarningFiltersBuilder warnerName(String reporterName) {
-            this.warningFilters.add(new SqlFilter<>(reporterName, Types.VARCHAR, "sp_warnings.warner_name"));
+            this.warningFilters.add(new SqlFilter<>(reporterName, Types.VARCHAR, "w.warner_name"));
             return this;
         }
 
         public WarningFiltersBuilder warner(SppPlayer assignee) {
-            this.warningFilters.add(new SqlFilter<>(assignee.getId().toString(), Types.VARCHAR, "sp_warnings.Warner_UUID"));
+            this.warningFilters.add(new SqlFilter<>(assignee.getId().toString(), Types.VARCHAR, "w.Warner_UUID"));
             return this;
         }
 
         public WarningFiltersBuilder reason(String reason) {
-            this.warningFilters.add(new SqlFilter<>(reason, Types.VARCHAR, "sp_warnings.Reason"));
+            this.warningFilters.add(new SqlFilter<>(reason, Types.VARCHAR, "w.Reason"));
             return this;
         }
 
         public WarningFiltersBuilder server(String server) {
-            this.warningFilters.add(new SqlFilter<>(server, Types.VARCHAR, "sp_warnings.server_name"));
+            this.warningFilters.add(new SqlFilter<>(server, Types.VARCHAR, "w.server_name"));
             return this;
         }
 
         public WarningFiltersBuilder createdAfter(long timestamp) {
-            this.warningFilters.add(new SqlFilter<>(timestamp, Types.BIGINT, "sp_warnings.timestamp", ">="));
+            this.warningFilters.add(new SqlFilter<>(timestamp, Types.BIGINT, "w.timestamp", ">="));
             return this;
         }
 
         public WarningFiltersBuilder expired(boolean expired) {
-            this.warningFilters.add(new SqlFilter<>(expired, Types.BOOLEAN, "sp_warnings.is_expired", "="));
+            this.warningFilters.add(new SqlFilter<>(expired, Types.BOOLEAN, "w.is_expired", "="));
             return this;
         }
 
