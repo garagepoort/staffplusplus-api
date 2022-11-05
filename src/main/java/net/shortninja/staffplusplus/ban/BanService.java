840943430c569d43ddd94f96e2e4f8d426f6e6bd
DavidMaes <david.s.maes@gmail.com>
2022-11-05 22:08:37 +0100

    Add ban functionality to API

--- src/main/java/net/shortninja/staffplusplus/ban/BanService.java
+++ src/main/java/net/shortninja/staffplusplus/ban/BanService.java
@@ -1,12 +1,39 @@
 package net.shortninja.staffplusplus.ban;
 
+import net.shortninja.staffplusplus.session.SppPlayer;
+import org.bukkit.command.CommandSender;
+
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface BanService {
+
+    void unban(CommandSender issuer, SppPlayer playerToUnban, String reason, boolean isSilent);
+
+    void unban(SppPlayer issuer, int banId, String reason);
 
     long getTotalBanCount();
 
     long getActiveBanCount();
+
+    void permBan(CommandSender issuer, SppPlayer playerToBan, String reason, String template, boolean isSilent);
+
+    void permBan(CommandSender issuer, SppPlayer playerToBan, String reason, boolean isSilent);
+
+    void tempBan(CommandSender issuer, SppPlayer playerToBan, Long durationInMillis, String reason, String template, boolean isSilent);
+
+    void tempBan(CommandSender issuer, SppPlayer playerToBan, Long durationInMillis, String reason, boolean isSilent);
+
+    void extendBan(CommandSender sender, SppPlayer player, long duration);
+
+    void reduceBan(CommandSender sender, SppPlayer player, long duration);
+
+    Optional<? extends IBan> getBanByBannedUuid(UUID playerUuid);
+
+    IBan getActiveById(int banId);
+
+    IBan getById(int banId);
 
     List<? extends IBan> getAllPaged(int offset, int amount);
 
